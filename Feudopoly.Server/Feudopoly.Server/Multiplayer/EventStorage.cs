60c3034b59edc445c17c0067a9e0f2c127489f34
Olexandr Kosovan <71893955+SkYMaaN@users.noreply.github.com>
2026-02-27 00:25:03 +0200

    Fix FillEvents roll event construction with dictor speech

--- Feudopoly.Server/Feudopoly.Server/Multiplayer/EventStorage.cs
+++ Feudopoly.Server/Feudopoly.Server/Multiplayer/EventStorage.cs
@@ -80,8 +80,10 @@ namespace Feudopoly.Server.Multiplayer
                     O(OutcomeKind.MoveByOffset, "Move forward 1 space.", moveOffset: 1)),
 
                 [14] = Roll("Children to monastery", "All your children go to a monastery. Roll again.",
-                    RO(RollResultKind.Lose, R(1, 3), O(OutcomeKind.SkipTurns, "You will never have grandchildren. Skip 2 moves. Religion also has disadvantages.", skipTurns: 2)),
-                    RO(RollResultKind.Tie, R(4, 6), O(OutcomeKind.None, "The children found the monastery too strenuous. We were lucky again!")),
+                    [
+                        RO(RollResultKind.Lose, R(1, 3), O(OutcomeKind.SkipTurns, "You will never have grandchildren. Skip 2 moves. Religion also has disadvantages.", skipTurns: 2)),
+                        RO(RollResultKind.Tie, R(4, 6), O(OutcomeKind.None, "The children found the monastery too strenuous. We were lucky again!"))
+                    ],
                     "Religion also has disadvantages."),
 
                 [15] = Fixed("Bullied kid becomes Pope", "The guy you beat up as a kid becomes Pope. You argue that a Christian must forgive. He promised to think about it. You flee on field 8.",
@@ -95,23 +97,29 @@ namespace Feudopoly.Server.Multiplayer
                     O(OutcomeKind.None, "Nothing happens."), "Rejoice."),
 
                 [18] = Roll("Malleus Maleficarum", "You are on the field Malleus Maleficarum. All women in the game have to roll the dice.",
-                    RO(RollResultKind.Lose, R(6, 6), O(OutcomeKind.Eliminate, "Anyone who rolls a 6 is out of the game.", OutcomeTarget.Women)),
-                    RO(RollResultKind.Tie, R(1, 5), O(OutcomeKind.None, "The others were lucky.", OutcomeTarget.Women)),
-                    "It's not called \"the Dark Ages\" for nothing."),
+                    [
+                        RO(RollResultKind.Lose, R(6, 6), O(OutcomeKind.Eliminate, "Anyone who rolls a 6 is out of the game.", OutcomeTarget.Women)),
+                        RO(RollResultKind.Tie, R(1, 5), O(OutcomeKind.None, "The others were lucky.", OutcomeTarget.Women))
+                    ],
+                    "It\'s not called \"the Dark Ages\" for nothing."),
 
                 [19] = Fixed("Looked out the window", "While others were learning to read, you were looking out the window (yes, for the entire 8 years). Go back to field 17.",
                     O(OutcomeKind.MoveToCell, "Move immediately to field 17.", moveToCell: 17)),
 
                 [20] = Roll("da Vinci helicopter", "Leonardo da Vinci draws a helicopter. Roll again.",
-                    RO(RollResultKind.Lose, R(1, 2), O(OutcomeKind.SkipTurns, "You think it's the devil's work -> suspend a turn. If you don't want progress, don't stop others from making it.", skipTurns: 1)),
-                    RO(RollResultKind.Tie, R(3, 4), O(OutcomeKind.None, "You don't care. Smile and be happy. You are not capable of more than that.")),
-                    RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.RepeatRoll, "You have difficulty forming your own opinion. Pass the cube on.")),
+                    [
+                        RO(RollResultKind.Lose, R(1, 2), O(OutcomeKind.SkipTurns, "You think it's the devil's work -> suspend a turn. If you don't want progress, don't stop others from making it.", skipTurns: 1)),
+                        RO(RollResultKind.Tie, R(3, 4), O(OutcomeKind.None, "You don't care. Smile and be happy. You are not capable of more than that.")),
+                        RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.RepeatRoll, "You have difficulty forming your own opinion. Pass the cube on."))
+                    ],
                     "If you don't want progress, don't stop others from making it."),
 
                 [21] = Roll("1492 America rediscovered", "It is the year 1492, America is rediscovered. Roll again.",
-                    RO(RollResultKind.Lose, R(1, 2), O(OutcomeKind.SkipTurns, "You're sad because you actually wanted Indian spices -> suspend a turn.", skipTurns: 1)),
-                    RO(RollResultKind.Tie, R(3, 4), O(OutcomeKind.None, "You set off but didn't arrive – the sea was simply too rough.")),
-                    RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.None, "You can't read, you know nothing about America -> smile and be glad. You are not capable of more than that.")),
+                    [
+                        RO(RollResultKind.Lose, R(1, 2), O(OutcomeKind.SkipTurns, "You're sad because you actually wanted Indian spices -> suspend a turn.", skipTurns: 1)),
+                        RO(RollResultKind.Tie, R(3, 4), O(OutcomeKind.None, "You set off but didn't arrive – the sea was simply too rough.")),
+                        RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.None, "You can't read, you know nothing about America -> smile and be glad. You are not capable of more than that."))
+                    ],
                     "Smile and be happy. It could be worse."),
 
                 [22] = Roll("The Reformation", "For some it's good, for others not. Roll again.",
@@ -138,14 +146,18 @@ namespace Feudopoly.Server.Multiplayer
                     O(OutcomeKind.MoveByOffset, "Move forward 2 spaces.", moveOffset: 2)),
 
                 [28] = Roll("Telescope discoveries", "You observe the sky with a telescope, discover many new things, and publish a book about it. The church takes notice of you. Roll again.",
-                    RO(RollResultKind.Tie, R(1, 2), O(OutcomeKind.SkipTurns, "You recant your discoveries and beg for mercy. Skip 1 move.", skipTurns: 1)),
-                    RO(RollResultKind.Lose, R(3, 4), O(OutcomeKind.Eliminate, "You will be accused of heresy and end up at the stake.")),
-                    RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.MoveByOffset, "You are revolutionizing science - go forward 3 spaces, you survived!", moveOffset: 3)),
+                    [
+                        RO(RollResultKind.Tie, R(1, 2), O(OutcomeKind.SkipTurns, "You recant your discoveries and beg for mercy. Skip 1 move.", skipTurns: 1)),
+                        RO(RollResultKind.Lose, R(3, 4), O(OutcomeKind.Eliminate, "You will be accused of heresy and end up at the stake.")),
+                        RO(RollResultKind.Win, R(5, 6), O(OutcomeKind.MoveByOffset, "You are revolutionizing science - go forward 3 spaces, you survived!", moveOffset: 3))
+                    ],
                     "You survived!"),
 
                 [29] = Roll("Industrialization begins", "Industrialization begins. Roll again.",
-                    RO(RollResultKind.Lose, R(1, 3), O(OutcomeKind.None, "Rejoice. You will be significantly better off in just 200 years because of this")),
-                    RO(RollResultKind.Win, R(4, 6), O(OutcomeKind.SkipTurns, "You prefer honest, manual labor in the field - even if you're pulling the plow yourself. Skip 2 moves", skipTurns: 2)),
+                    [
+                        RO(RollResultKind.Lose, R(1, 3), O(OutcomeKind.None, "Rejoice. You will be significantly better off in just 200 years because of this")),
+                        RO(RollResultKind.Win, R(4, 6), O(OutcomeKind.SkipTurns, "You prefer honest, manual labor in the field - even if you're pulling the plow yourself. Skip 2 moves", skipTurns: 2))
+                    ],
                     "Rejoice.")
             };
 
