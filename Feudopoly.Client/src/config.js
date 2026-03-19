1435c30123db4731de4919ba77b3e2872b7ae4fc
AKO <sashakosovan4@gmail.com>
2026-03-19 04:49:02 +0200

    Update backendBaseUrl for production

--- Feudopoly.Client/src/config.js
+++ Feudopoly.Client/src/config.js
@@ -1,4 +1,4 @@
-export const backendBaseUrl = 'https://localhost:7049';
+export const backendBaseUrl = 'https://feudopolyserver.orangesea-b37f54d7.westeurope.azurecontainerapps.io';
 
 // https://localhost:7049
 // https://feudopolyserver.orangesea-b37f54d7.westeurope.azurecontainerapps.io
