0d47ea907f4cdcdd47bcf962b665d5ae652a098f
AKO <sashakosovan4@gmail.com>
2026-06-01 21:00:10 +0300

    Fixed base url for UI

--- Feudopoly.Client/src/config.js
+++ Feudopoly.Client/src/config.js
@@ -1,4 +1,4 @@
-export const backendBaseUrl = 'https://localhost:7049';
+export const backendBaseUrl = 'https://feudopolyserver.orangesea-b37f54d7.westeurope.azurecontainerapps.io';
 const normalizedBackendBaseUrl = backendBaseUrl.replace(/\/+$/, '');
 export const videoBaseUrl = `${normalizedBackendBaseUrl}/api/videos`;
 export const AUTO_TURN_TIMEOUT_MS = 30000;
